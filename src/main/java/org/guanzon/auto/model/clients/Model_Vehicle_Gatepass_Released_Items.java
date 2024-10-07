18c0e1e43331d7f430e9dee176abbe6f022ba230
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-10-07 17:11:09 +0800

    10072024 - Added functionality for vehicle gatepass release items
    10072024 - Added functionality for vehicle gatepass release items

---
+++ src/main/java/org/guanzon/auto/model/clients/Model_Vehicle_Gatepass_Released_Items.java
@@ -0,0 +1,584 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.guanzon.auto.model.clients;
+
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import javax.sql.rowset.CachedRowSet;
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.appdriver.base.SQLUtil;
+import org.guanzon.appdriver.constant.EditMode;
+import org.guanzon.appdriver.constant.TransactionStatus;
+import org.guanzon.appdriver.iface.GEntity;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author Arsiela
+ */
+public class Model_Vehicle_Gatepass_Released_Items implements GEntity {
+    final String XML = "Model_Vehicle_Gatepass_Released_Items.xml";
+    private final String psDefaultDate = "1900-01-01";
+    private String psTargetBranchCd;
+    private String psExclude = "sLaborDsc»sStockDsc"; //»
+    
+    GRider poGRider;                //application driver
+    CachedRowSet poEntity;          //rowset
+    JSONObject poJSON;              //json container
+    int pnEditMode;                 //edit mode
+
+    /**
+     * Entity constructor
+     *
+     * @param foValue - GhostRider Application Driver
+     */
+    public Model_Vehicle_Gatepass_Released_Items(GRider foValue) {
+        if (foValue == null) {
+            System.err.println("Application Driver is not set.");
+            System.exit(1);
+        }
+
+        poGRider = foValue;
+
+        initialize();
+    }
+    
+    private void initialize() {
+        try {
+            poEntity = MiscUtil.xml2ResultSet(System.getProperty("sys.default.path.metadata") + XML, getTable());
+
+            poEntity.last();
+            poEntity.moveToInsertRow();
+
+            MiscUtil.initRowSet(poEntity);        
+            poEntity.updateInt("nQuantity", 0);
+            poEntity.updateInt("nReleased", 0);
+            
+            poEntity.insertRow();
+            poEntity.moveToCurrentRow();
+            poEntity.absolute(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Gets the column index name.
+     *
+     * @param fnValue - column index number
+     * @return column index name
+     */
+    @Override
+    public String getColumn(int fnValue) {
+        try {
+            return poEntity.getMetaData().getColumnLabel(fnValue);
+        } catch (SQLException e) {
+        }
+        return "";
+    }
+
+    /**
+     * Gets the column index number.
+     *
+     * @param fsValue - column index name
+     * @return column index number
+     */
+    @Override
+    public int getColumn(String fsValue) {
+        try {
+            return MiscUtil.getColumnIndex(poEntity, fsValue);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Gets the total number of column.
+     *
+     * @return total number of column
+     */
+    @Override
+    public int getColumnCount() {
+        try {
+            return poEntity.getMetaData().getColumnCount();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int getEditMode() {
+        return pnEditMode;
+    }
+
+    @Override
+    public String getTable() {
+        return "vehicle_released_items";
+    }
+    
+    /**
+     * Gets the value of a column index number.
+     *
+     * @param fnColumn - column index number
+     * @return object value
+     */
+    @Override
+    public Object getValue(int fnColumn) {
+        try {
+            return poEntity.getObject(fnColumn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Gets the value of a column index name.
+     *
+     * @param fsColumn - column index name
+     * @return object value
+     */
+    @Override
+    public Object getValue(String fsColumn) {
+        try {
+            return poEntity.getObject(MiscUtil.getColumnIndex(poEntity, fsColumn));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Sets column value.
+     *
+     * @param fnColumn - column index number
+     * @param foValue - value
+     * @return result as success/failed
+     */
+    @Override
+    public JSONObject setValue(int fnColumn, Object foValue) {
+        try {
+            poJSON = MiscUtil.validateColumnValue(System.getProperty("sys.default.path.metadata") + XML, MiscUtil.getColumnLabel(poEntity, fnColumn), foValue);
+            if ("error".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
+
+            poEntity.updateObject(fnColumn, foValue);
+            poEntity.updateRow();
+
+            poJSON = new JSONObject();
+            poJSON.put("result", "success");
+            poJSON.put("value", getValue(fnColumn));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            poJSON.put("result", "error");
+            poJSON.put("message", e.getMessage());
+        }
+
+        return poJSON;
+    }
+    
+    /**
+     * Sets column value.
+     *
+     * @param fsColumn - column index name
+     * @param foValue - value
+     * @return result as success/failed
+     */
+    @Override
+    public JSONObject setValue(String fsColumn, Object foValue) {
+        poJSON = new JSONObject();
+
+        try {
+            return setValue(MiscUtil.getColumnIndex(poEntity, fsColumn), foValue);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            poJSON.put("result", "error");
+            poJSON.put("message", e.getMessage());
+        }
+        return poJSON;
+    }
+
+    /**
+     * Set the edit mode of the entity to new.
+     *
+     * @return result as success/failed
+     */
+    @Override
+    public JSONObject newRecord() {
+        pnEditMode = EditMode.ADDNEW;
+        
+        poJSON = new JSONObject();
+        poJSON.put("result", "success");
+        return poJSON;
+    }
+    
+    
+    @Override
+    public JSONObject openRecord(String fsValue) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    /**
+     * Opens a record.
+     *
+     * @param fsValue - filter values
+     * @return result as success/failed
+     */
+    public JSONObject openRecord(String fsValue, String fsValue2) {
+        poJSON = new JSONObject();
+
+        String lsSQL = getSQL(); //MiscUtil.makeSelect(this, psExclude); //exclude the columns called thru left join
+        //replace the condition based on the primary key column of the record
+        lsSQL = MiscUtil.addCondition(lsSQL, " a.sTransNox = " + SQLUtil.toSQL(fsValue)
+                                                + " AND (sLaborCde = " + SQLUtil.toSQL(fsValue2)
+                                                + " OR sStockIDx = " + SQLUtil.toSQL(fsValue2) + " ) "
+                                                );
+
+        System.out.println(lsSQL);
+        ResultSet loRS = poGRider.executeQuery(lsSQL);
+
+        try {
+            if (loRS.next()) {
+                for (int lnCtr = 1; lnCtr <= loRS.getMetaData().getColumnCount(); lnCtr++) {
+                    setValue(lnCtr, loRS.getObject(lnCtr));
+                }
+
+                pnEditMode = EditMode.UPDATE;
+
+                poJSON.put("result", "success");
+                poJSON.put("message", "Record loaded successfully.");
+            } else {
+                poJSON.put("result", "error");
+                poJSON.put("message", "No record to load.");
+            }
+        } catch (SQLException e) {
+            poJSON.put("result", "error");
+            poJSON.put("message", e.getMessage());
+        }
+
+        return poJSON;
+    }
+
+    /**
+     * Save the entity.
+     *
+     * @return result as success/failed
+     */
+    @Override
+    public JSONObject saveRecord() {
+        poJSON = new JSONObject();
+
+        if (pnEditMode == EditMode.ADDNEW || pnEditMode == EditMode.UPDATE) {
+            String lsSQL; 
+            if (pnEditMode == EditMode.ADDNEW) {
+                
+                lsSQL = MiscUtil.makeSQL(this, psExclude);
+                
+               // lsSQL = "Select * FROM " + getTable() + " a left join (" + makeSQL() + ") b on a.column1 = b.column "
+                if (!lsSQL.isEmpty()) {
+                    if (poGRider.executeQuery(lsSQL, getTable(), poGRider.getBranchCode(), psTargetBranchCd) > 0) {
+                        poJSON.put("result", "success");
+                        poJSON.put("message", "Record saved successfully.");
+                    } else {
+                        poJSON.put("result", "error");
+                        poJSON.put("message", poGRider.getErrMsg());
+                    }
+                } else {
+                    poJSON.put("result", "error");
+                    poJSON.put("message", "No record to save.");
+                }
+            } else {
+                Model_Vehicle_Gatepass_Released_Items loOldEntity = new Model_Vehicle_Gatepass_Released_Items(poGRider);
+                
+                //replace with the primary key column info
+                JSONObject loJSON = new JSONObject();
+                switch(this.getItemType()){
+                    case "l":
+                        loJSON = loOldEntity.openRecord(this.getTransNo(),this.getLaborCde());
+                    break;
+                    case "p":
+                        loJSON = loOldEntity.openRecord(this.getTransNo(),this.getStockID());
+                    break;
+                    case "d":
+                    break;
+                }
+
+                if ("success".equals((String) loJSON.get("result"))) {
+                    
+                    //replace the condition based on the primary key column of the record
+                    lsSQL = MiscUtil.makeSQL(this, loOldEntity, "sTransNox = " + SQLUtil.toSQL(this.getTransNo()), psExclude);
+
+                    if (!lsSQL.isEmpty()) {
+                        if (poGRider.executeQuery(lsSQL, getTable(), poGRider.getBranchCode(), psTargetBranchCd) > 0) {
+                            poJSON.put("result", "success");
+                            poJSON.put("message", "Record saved successfully.");
+                        } else {
+                            poJSON.put("result", "error");
+                            poJSON.put("message", poGRider.getErrMsg());
+                        }
+                    } else {
+                        poJSON.put("result", "success");
+                        poJSON.put("message", "No updates has been made.");
+                    }
+                } else {
+                    poJSON.put("result", "error");
+                    poJSON.put("message", "Record discrepancy. Unable to save record.");
+                }
+            }
+        } else {
+            poJSON.put("result", "error");
+            poJSON.put("message", "Invalid update mode. Unable to save record.");
+            return poJSON;
+        }
+
+        return poJSON;
+    }
+    
+    public void setTargetBranchCd(String fsBranchCd){
+        if (!poGRider.getBranchCode().equals(fsBranchCd)){
+            psTargetBranchCd = fsBranchCd;
+        } else {
+            psTargetBranchCd = "";
+        }
+    }
+    
+    public JSONObject deleteRecord(){
+        poJSON = new JSONObject();
+        
+        String lsSQL = " DELETE FROM "+getTable()+" WHERE "
+                    + " sTransNox = " + SQLUtil.toSQL(this.getTransNo())
+                    + " AND sLaborCde = " + SQLUtil.toSQL(this.getLaborCde())
+                    + " AND sStockIDx = " + SQLUtil.toSQL(this.getStockID());
+        if (!lsSQL.isEmpty()) {
+            if (poGRider.executeQuery(lsSQL, getTable(), poGRider.getBranchCode(), psTargetBranchCd) > 0) {
+                poJSON.put("result", "success");
+                poJSON.put("message", "Record deleted successfully.");
+            } else {
+                poJSON.put("result", "error");
+                poJSON.put("continue", true);
+                poJSON.put("message", poGRider.getErrMsg());
+            }
+        }
+        return poJSON;
+    }
+
+    /**
+     * Prints all the public methods used<br>
+     * and prints the column names of this entity.
+     */
+    @Override
+    public void list() {
+        Method[] methods = this.getClass().getMethods();
+
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("LIST OF PUBLIC METHODS FOR " + this.getClass().getName() + ":");
+        System.out.println("--------------------------------------------------------------------");
+        for (Method method : methods) {
+            System.out.println(method.getName());
+        }
+
+        try {
+            int lnRow = poEntity.getMetaData().getColumnCount();
+
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("ENTITY COLUMN INFO");
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("Total number of columns: " + lnRow);
+            System.out.println("--------------------------------------------------------------------");
+
+            for (int lnCtr = 1; lnCtr <= lnRow; lnCtr++) {
+                System.out.println("Column index: " + (lnCtr) + " --> Label: " + poEntity.getMetaData().getColumnLabel(lnCtr));
+                if (poEntity.getMetaData().getColumnType(lnCtr) == Types.CHAR
+                        || poEntity.getMetaData().getColumnType(lnCtr) == Types.VARCHAR) {
+
+                    System.out.println("Column index: " + (lnCtr) + " --> Size: " + poEntity.getMetaData().getColumnDisplaySize(lnCtr));
+                }
+            }
+        } catch (SQLException e) {
+        }
+
+    }
+    
+    /**
+     * Gets the SQL statement for this entity.
+     *
+     * @return SQL Statement
+     */
+    public String makeSQL() {
+        return MiscUtil.makeSQL(this, psExclude);
+    }
+    
+    /**
+     * Gets the SQL Select statement for this entity.
+     *
+     * @return SQL Select Statement
+     */
+    public String makeSelectSQL() {
+        return MiscUtil.makeSelect(this, psExclude);
+    }
+    
+    public String getSQL(){
+        return    " SELECT "                                             
+                + "    a.sTransNox "                                     
+                + "  , a.sItemType "                                     
+                + "  , a.sLaborCde "                                     
+                + "  , a.sStockIDx "                                     
+                + "  , a.nQuantity "                                     
+                + "  , a.nReleased "                                     
+                + "  , b.sLaborDsc "                                     
+                + "  , c.sDescript AS sStockDsc "                                     
+                + " FROM vehicle_released_items a "                      
+                + " LEFT JOIN labor b ON b.sLaborCde = a.sLaborCde "     
+                + " LEFT JOIN inventory c ON c.sStockIDx = a.sStockIDx "  ;                          
+    }
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setTransNo(String fsValue) {
+        return setValue("sTransNox", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getTransNo() {
+        return (String) getValue("sTransNox");
+    }
+    
+    /**
+     * Description: Sets the Value of this record.
+     *
+     * @param fsValue
+     * @return True if the record assignment is successful.
+     */
+    public JSONObject setItemType(String fsValue) {
+        return setValue("sItemType", fsValue);
+    }
+
+    /**
+     * @return The Value of this record.
+     */
+    public String getItemType() {
+        return (String) getValue("sItemType");
+    }
+    
+    /**
+     * Description: Sets the Value of this record.
+     *
+     * @param fsValue
+     * @return True if the record assignment is successful.
+     */
+    public JSONObject setLaborCde(String fsValue) {
+        return setValue("sLaborCde", fsValue);
+    }
+
+    /**
+     * @return The Value of this record.
+     */
+    public String getLaborCde() {
+        return (String) getValue("sLaborCde");
+    }
+    
+    /**
+     * Description: Sets the Value of this record.
+     *
+     * @param fsValue
+     * @return True if the record assignment is successful.
+     */
+    public JSONObject setStockID(String fsValue) {
+        return setValue("sStockIDx", fsValue);
+    }
+
+    /**
+     * @return The Value of this record.
+     */
+    public String getStockID() {
+        return (String) getValue("sStockIDx");
+    }
+    
+    /**
+     * Description: Sets the Value of this record.
+     *
+     * @param fnValue
+     * @return result as success/failed
+     */
+    public JSONObject setQuantity(Integer fnValue) {
+        return setValue("nQuantity", fnValue);
+    }
+
+    /**
+     * @return The Value of this record.
+     */
+    public Integer getQuantity() {
+        return Integer.parseInt(String.valueOf(getValue("nQuantity")));
+    }
+    
+    /**
+     * Description: Sets the Value of this record.
+     *
+     * @param fnValue
+     * @return result as success/failed
+     */
+    public JSONObject setReleased(Integer fnValue) {
+        return setValue("nReleased", fnValue);
+    }
+
+    /**
+     * @return The Value of this record.
+     */
+    public Integer getReleased() {
+        return Integer.parseInt(String.valueOf(getValue("nReleased")));
+    }
+    
+    /**
+     * Description: Sets the Value of this record.
+     *
+     * @param fsValue
+     * @return True if the record assignment is successful.
+     */
+    public JSONObject setLaborDsc(String fsValue) {
+        return setValue("sLaborDsc", fsValue);
+    }
+
+    /**
+     * @return The Value of this record.
+     */
+    public String getLaborDsc() {
+        return (String) getValue("sLaborDsc");
+    }
+    
+    /**
+     * Description: Sets the Value of this record.
+     *
+     * @param fsValue
+     * @return True if the record assignment is successful.
+     */
+    public JSONObject setStockDsc(String fsValue) {
+        return setValue("sStockDsc", fsValue);
+    }
+
+    /**
+     * @return The Value of this record.
+     */
+    public String getStockDsc() {
+        return (String) getValue("sStockDsc");
+    }
+}
